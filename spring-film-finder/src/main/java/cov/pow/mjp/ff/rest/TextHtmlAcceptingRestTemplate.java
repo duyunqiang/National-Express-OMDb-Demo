85d8c23a7cef0b7734f846cc3fc39ade48aab77c
matthew-pearson <matthew.pearson@linuxmail.org>
2015-03-31 19:37:36 +0100

    Add support for application/json
    because omdbapi.com has now started to declare it's
    response correctly like this, instead of text/html

--- spring-film-finder/src/main/java/cov/pow/mjp/ff/rest/TextHtmlAcceptingRestTemplate.java
+++ spring-film-finder/src/main/java/cov/pow/mjp/ff/rest/TextHtmlAcceptingRestTemplate.java
@@ -1,6 +1,5 @@
 package cov.pow.mjp.ff.rest;
 
-import java.util.Collections;
 import java.util.List;
 
 import org.apache.http.client.HttpClient;
@@ -12,29 +11,23 @@ import org.springframework.web.client.RestTemplate;
 
 /**
  * omdbapi.com returns json, but declares it as text in the Content-Type header. So we need to extend Spring's RestTemplate.
- * This only supports conversion of text if it's really json, but declares itself as text/html instead of application/json. 
+ * This additionally supports conversion of text if it's really json, but declares itself as text/html instead of application/json. 
  * @author Mat
  */
 public class TextHtmlAcceptingRestTemplate extends RestTemplate {
-	
-	private List<HttpMessageConverter<?>> messageConvertors = 
-			Collections.<HttpMessageConverter<?>>singletonList(new TextHtmlToJsonMessageConverter());
 
 	public TextHtmlAcceptingRestTemplate() {
 		// Also deal with gzipped responses.
 		super(createGzipRequestFactory());
-		setMessageConverters(getMessageConverters());
+		List<HttpMessageConverter<?>> messageConverters = getMessageConverters();
+		messageConverters.add(new TextHtmlToJsonMessageConverter());
+		setMessageConverters(messageConverters);
 	}
 
 	private static ClientHttpRequestFactory createGzipRequestFactory() {
 		HttpClient httpClient = HttpClientBuilder.create().build();
 		ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
 		return requestFactory;
-	}
-	
-	@Override
-	public List<HttpMessageConverter<?>> getMessageConverters() {
-		return messageConvertors;
 	}
 
 }
